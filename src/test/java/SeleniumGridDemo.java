e5280f6e2b8cc7f6ac367abb095b752c8a5c0ab0
David Halison <halison700@gmail.com>
2021-08-26 23:11:26 -0300

    Criando testes em parallel

--- src/test/java/SeleniumGridDemo.java
+++ src/test/java/SeleniumGridDemo.java
@@ -13,15 +13,37 @@ public class SeleniumGridDemo {
     public void test1() {
 
         try {
+            System.out.println("Test1 started...");
             DesiredCapabilities cap = new DesiredCapabilities();
             cap.setBrowserName("chrome");
             cap.setPlatform(Platform.LINUX);
 
             WebDriver driver = new RemoteWebDriver(new URL("http://172.18.0.1:4444/wd/hub"), cap);
-            driver.manage().window().maximize();
+//            driver.manage().window().maximize();
             driver.get("https://www.google.com/");
             System.out.println("Title of the page is " + driver.getTitle());
             driver.quit();
+            System.out.println("Test1 ended...");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test2() {
+
+        try {
+            System.out.println("Test2 started...");
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.setBrowserName("chrome");
+            cap.setPlatform(Platform.LINUX);
+
+            WebDriver driver = new RemoteWebDriver(new URL("http://172.18.0.1:4444/wd/hub"), cap);
+//            driver.manage().window().maximize();
+            driver.get("https://www.google.com/");
+            System.out.println("Title of the page is " + driver.getTitle());
+            driver.quit();
+            System.out.println("Test2 ended...");
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
